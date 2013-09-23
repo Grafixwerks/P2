5b984fb9a8b0da563c69070b88c1331df9abfd46
Andrew Pearson <Andy@Grafixwerks.com>
2013-09-23 03:34:42 -0400

    HTML & CSS for dashboard

---
+++ dashboard.php
@@ -0,0 +1,103 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<!-- CSCI E-15 | Andrew Pearson | P 2 -->
+<meta charset="utf-8">
+<title>Holy Tweet</title>
+<link href='http://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+
+<body>
+<div class="main-nav">
+  <ul class="nav">
+    <li><a href="" title="Account" id="acct">Account</a>
+      <ul class="dropdown">
+        <li><a href="">View profile</a></li>
+        <li><a href="">Account dashboard</a></li>
+        <li><a href="">Log out </a></li>
+      </ul><!-- .dropdown --> 
+      
+      
+    </li>
+    <li><a href="" title="Home" id="home">Home</a></li>
+  </ul><!-- .nav --> 
+  
+</div><!-- .main-nav -->
+
+
+<div class="wrapper"> <a href="/" class="logo">Holy Tweet: from your keyboard to God’s ear!</a>
+  <div class="main-content">
+    <h1>Welcome brother Ned</h1>
+
+
+
+
+<!--Now let us upload the holy Tweet of the Lord.--> 
+
+
+<form action="" method="post" enctype="application/x-www-form-urlencoded" id="send-tweet">
+  <fieldset>
+    <legend>Create your tweet...</legend>
+
+      <label for="tweet-msg" class="tweet-label">Message:</label>
+      <input type="text" name="tweet-msg" id="tweet-msg"  class="tweet-txt" maxlength="140" />
+  <input type="submit" name="submit" value="Tweet" id="btn-tweet" class="btn" />    
+  </fieldset>
+
+  <br class="clr-flt">
+</form>
+
+ 
+<ul class="profile-nav">
+<li><span class="count">3</span> <a href="">Tweets</a></li>
+<li><span class="count">8</span> <a href="">Following</a></li>
+<li><span class="count">6</span> <a href="">Followers</a></li>
+</ul><!-- .profile-nav -->
+
+
+    <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
+      <div class="tweet-left">
+        <h3><a href="#">Daisuke Murase</a> @typester</h3>
+        <p>Pellentesque tincidunt velit ac tellus malesuada, vel laoreet lectus dignissim.  Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. </p>
+      </div><!-- .tweet-left -->
+      
+      <div class="tweet-right"><span class="date">18 Sep</span> <a href="#" class="btn-follow">unfollow</a> </div><!-- .tweet-right --> 
+      <br class="clr-flt">
+    </div><!-- .tweet --> 
+
+
+    <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
+      <div class="tweet-left">
+        <h3><a href="#">Daisuke Murase</a> @typester</h3>
+        <p>Pellentesque tincidunt velit ac tellus malesuada, vel laoreet lectus dignissim.  Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. </p>
+      </div><!-- .tweet-left -->
+      
+      <div class="tweet-right"><span class="date">18 Sep</span> <a href="#" class="btn-follow">unfollow</a> </div><!-- .tweet-right --> 
+      <br class="clr-flt">
+    </div><!-- .tweet --> 
+
+
+
+    <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
+      <div class="tweet-left">
+        <h3><a href="#">Daisuke Murase</a> @typester</h3>
+        <p>Pellentesque tincidunt velit ac tellus malesuada, vel laoreet lectus dignissim.  Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. </p>
+      </div><!-- .tweet-left -->
+      
+      <div class="tweet-right"><span class="date">18 Sep</span> <a href="#" class="btn-follow">unfollow</a> </div><!-- .tweet-right --> 
+      <br class="clr-flt">
+    </div><!-- .tweet --> 
+
+
+
+
+    
+    
+  </div><!-- .main-content -->
+   
+</div><!-- .wrapper -->
+
+
+</body>
+</html>

8ead73e69d47fb08759f09cf9159652d13eb40ab
Andrew Pearson <Andy@Grafixwerks.com>
2013-09-22 22:27:21 -0400

    HTML template - logo, navigation, main layout

--- index.php
+++ index.php
@@ -1,15 +1,36 @@
 <!DOCTYPE HTML>
 <html>
 <head>
+<!-- CSCI E-15 | Andrew Pearson | P 2 -->
 <meta charset="utf-8">
-<title>Untitled Document</title>
-
+<title>Holy Tweet</title>
+<link href='http://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="css/main.css">
-
 </head>
 
 <body>
+<div class="main-nav">
+  <ul class="nav">
+    <li><a href="" title="Account" id="acct">Account</a>
+      <ul class="dropdown">
+        <li><a href="">View profile</a></li>
+        <li><a href="">Account dashboard</a></li>
+        <li><a href="">Log out  </a></li>
+      </ul><!-- .dropdown --> 
 
+    </li>
+    <li><a href="" title="Home" id="home">Home</a></li>
+  </ul><!-- .nav --> 
+</div><!-- .main-nav -->
+
+
+<div class="wrapper"> <a href="/" class="logo">Holy Tweet: from your keyboard to God’s ear!</a>
+  <div class="main-content">
+    <h1>Tweets</h1>
+  </div><!-- .main-content --> 
+  
+  
+</div><!-- .wrapper -->
 
 
 </body>

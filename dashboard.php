5a5b8c5e43f1b237ec2ebee91aefc3e7c80b8436
Andrew Pearson <Andy@Grafixwerks.com>
2013-09-23 18:36:48 -0400

    JS for tabs in dashboard

--- dashboard.php
+++ dashboard.php
@@ -51,10 +51,14 @@
 
  
 <ul class="profile-nav">
-<li><span class="count">3</span> <a href="">Tweets</a></li>
-<li><span class="count">8</span> <a href="">Following</a></li>
-<li class="last"><span class="count">6</span> <a href="">Followers</a></li>
+<li><span class="count">3</span> <a href="#" class="tweets-tab">Tweets</a></li>
+<li><span class="count">8</span> <a href="#" class="following-tab">Following</a></li>
+<li class="last"><span class="count">6</span> <a href="#" class="followers-tab">Followers</a></li>
 </ul><!-- .profile-nav -->
+
+
+    
+<div class="tab-content" id="tab-one">
 
 
 <h2>Your tweets</h2>
@@ -94,6 +98,56 @@
       <br class="clr-flt">
     </div><!-- .tweet --> 
 
+</div><!-- #tab-one -->
+
+
+
+<div class="tab-content" id="tab-two">
+    <h2>Following</h2>
+
+
+    <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
+      <div class="tweet-left">
+        <h3><a href="#">Daisuke Murase</a> @typester</h3>
+        <p>Pellentesque tincidunt velit ac tellus malesuada, vel laoreet lectus dignissim.  Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. </p>
+      </div><!-- .tweet-left -->
+      
+      <div class="tweet-right"><span class="date">18 Sep</span> <a href="#" class="btn-follow">unfollow</a> </div><!-- .tweet-right --> 
+      <br class="clr-flt">
+    </div><!-- .tweet --> 
+
+
+    <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
+      <div class="tweet-left">
+        <h3><a href="#">Daisuke Murase</a> @typester</h3>
+        <p>Pellentesque tincidunt velit ac tellus malesuada, vel laoreet lectus dignissim.  Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. </p>
+      </div><!-- .tweet-left -->
+      
+      <div class="tweet-right"><span class="date">18 Sep</span> <a href="#" class="btn-follow">unfollow</a> </div><!-- .tweet-right --> 
+      <br class="clr-flt">
+    </div><!-- .tweet --> 
+
+
+
+    <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
+      <div class="tweet-left">
+        <h3><a href="#">Daisuke Murase</a> @typester</h3>
+        <p>Pellentesque tincidunt velit ac tellus malesuada, vel laoreet lectus dignissim.  Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. </p>
+      </div><!-- .tweet-left -->
+      
+      <div class="tweet-right"><span class="date">18 Sep</span> <a href="#" class="btn-follow">unfollow</a> </div><!-- .tweet-right --> 
+      <br class="clr-flt">
+    </div><!-- .tweet --> 
+
+
+
+</div><!-- #tab-2 -->
+
+
+<div class="tab-content" id="tab-three">
+tab 3
+</div><!-- #tab-3 -->
+<!-- style="display:none"-->
 
   </div><!-- .main-content -->
    
@@ -110,31 +164,25 @@ $(document).ready(function() {
 // These show and hide tabs
 
 // Hide things on load
-      $('#tab-2').hide();
-      $('#tab-3').hide();
+      $('#tab-two').hide();
+      $('#tab-three').hide();
 
 // show/hide on pagination link click
     $('.tweets-tab').click(function () {
       $('.tab-content').hide(); 											 
       $('#tab-one').show();
+	  (this).preventDefault();
     });
     $('.following-tab').click(function () {
       $('.tab-content').hide(); 											 
       $('#tab-two').show();
+	  (this).preventDefault();
     });
     $('.followers-tab').click(function () {
       $('.tab-content').hide(); 											 
       $('#tab-three').show();
+	  (this).preventDefault();
     });
-
-
-// Change selected tab 
-    $('.tabs').click(function () {
-      $('.tabs').removeClass("current");
-      $(this).addClass("current");
-//	  return false;
-    });
-
 
 }); // close doc ready
 

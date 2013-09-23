df1680fbeae38cb68926efc3b812973ccf27ccdf
Andrew Pearson <Andy@Grafixwerks.com>
2013-09-23 19:56:39 -0400

    Fixed HTML for 3rd tab

--- dashboard.php
+++ dashboard.php
@@ -6,7 +6,6 @@
 <title>Holy Tweet</title>
 <link href='http://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="css/main.css">
-
 </head>
 
 <body>
@@ -17,174 +16,172 @@
         <li><a href="">View profile</a></li>
         <li><a href="">Account dashboard</a></li>
         <li><a href="">Log out </a></li>
-      </ul><!-- .dropdown --> 
-      
+      </ul>
+      <!-- .dropdown --> 
       
     </li>
     <li><a href="" title="Home" id="home">Home</a></li>
-  </ul><!-- .nav --> 
+  </ul>
+  <!-- .nav --> 
   
-</div><!-- .main-nav -->
-
+</div>
+<!-- .main-nav -->
 
 <div class="wrapper"> <a href="/" class="logo">Holy Tweet: from your keyboard to God’s ear!</a>
   <div class="main-content">
     <h1>Welcome brother Ned</h1>
-
-
-
-
-
-
-
-<form action="" method="post" enctype="application/x-www-form-urlencoded" id="send-tweet">
-  <fieldset>
-    <legend>Create your tweet...</legend>
-
-      <label for="tweet-msg" class="tweet-label">Message:</label>
-      <input type="text" name="tweet-msg" id="tweet-msg"  class="tweet-txt" maxlength="140" />
-  <input type="submit" name="submit" value="Tweet" id="btn-tweet" class="btn" />    
-  </fieldset>
-
-  <br class="clr-flt">
-</form>
-
- 
-<ul class="profile-nav">
-<li><span class="count">3</span> <a href="#" class="tweets-tab">Tweets</a></li>
-<li><span class="count">8</span> <a href="#" class="following-tab">Following</a></li>
-<li class="last"><span class="count">6</span> <a href="#" class="followers-tab">Followers</a></li>
-</ul><!-- .profile-nav -->
-
-
+    <form action="" method="post" enctype="application/x-www-form-urlencoded" id="send-tweet">
+      <fieldset>
+        <legend>Create your tweet...</legend>
+        <label for="tweet-msg" class="tweet-label">Message:</label>
+        <input type="text" name="tweet-msg" id="tweet-msg"  class="tweet-txt" maxlength="140" />
+        <input type="submit" name="submit" value="Tweet" id="btn-tweet" class="btn" />
+      </fieldset>
+      <br class="clr-flt">
+    </form>
+    <ul class="profile-nav">
+      <li><span class="count">3</span> <a href="#" class="tweets-tab">Tweets</a></li>
+      <li><span class="count">8</span> <a href="#" class="following-tab">Following</a></li>
+      <li class="last"><span class="count">6</span> <a href="#" class="followers-tab">Followers</a></li>
+    </ul>
+    <!-- .profile-nav -->
     
-<div class="tab-content" id="tab-one">
-
-
-<h2>Your tweets</h2>
-
-
-    <div class="tweet">
-      <div class="tweet-left">
-
+    <div class="tab-content" id="tab-one">
+      <h2>Your tweets</h2>
+      <div class="tweet">
+        <div class="tweet-left">
+          <p>Pellentesque tincidunt velit ac tellus malesuada, vel laoreet lectus dignissim.  Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. </p>
+        </div>
+        <!-- .tweet-left -->
         
-        <p>Pellentesque tincidunt velit ac tellus malesuada, vel laoreet lectus dignissim.  Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. </p>
-      </div><!-- .tweet-left -->
+        <div class="tweet-right"><span class="date">18 Sep</span> <a href="#" class="btn-small edit">edit</a> </div>
+        <!-- .tweet-right --> 
+        <br class="clr-flt">
+      </div>
+      <!-- .tweet -->
       
-      <div class="tweet-right"><span class="date">18 Sep</span> <a href="#" class="btn-small edit">edit</a> </div><!-- .tweet-right --> 
-      <br class="clr-flt">
-    </div><!-- .tweet --> 
-
-
-    <div class="tweet">
-      <div class="tweet-left">
-
-        <p>Nullam ultricies ultrices pharetra. Pellentesque tincidunt velit ac tellus malesuada, vel laoreet lectus dignissim.</p>
-      </div><!-- .tweet-left -->
+      <div class="tweet">
+        <div class="tweet-left">
+          <p>Nullam ultricies ultrices pharetra. Pellentesque tincidunt velit ac tellus malesuada, vel laoreet lectus dignissim.</p>
+        </div>
+        <!-- .tweet-left -->
+        
+        <div class="tweet-right"><span class="date">14 Sep</span> <a href="#" class="btn-small edit">edit</a> </div>
+        <!-- .tweet-right --> 
+        <br class="clr-flt">
+      </div>
+      <!-- .tweet -->
       
-      <div class="tweet-right"><span class="date">14 Sep</span>  <a href="#" class="btn-small edit">edit</a> </div><!-- .tweet-right --> 
-      <br class="clr-flt">
-    </div><!-- .tweet --> 
-
-
-
-    <div class="tweet">
-      <div class="tweet-left">
-
-        <p>Sed dictum, est ac viverra porttitor, est leo lacinia massa.  Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. </p>
-      </div><!-- .tweet-left -->
+      <div class="tweet">
+        <div class="tweet-left">
+          <p>Sed dictum, est ac viverra porttitor, est leo lacinia massa.  Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. </p>
+        </div>
+        <!-- .tweet-left -->
+        
+        <div class="tweet-right"><span class="date">10 Sep</span> <a href="#" class="btn-small edit">edit</a> </div>
+        <!-- .tweet-right --> 
+        <br class="clr-flt">
+      </div>
+      <!-- .tweet --> 
       
-      <div class="tweet-right"><span class="date">10 Sep</span>  <a href="#" class="btn-small edit">edit</a> </div><!-- .tweet-right --> 
-      <br class="clr-flt">
-    </div><!-- .tweet --> 
-
-</div><!-- #tab-one -->
-
-
-
-<div class="tab-content" id="tab-two">
-    <h2>Following</h2>
-
-
-    <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
-      <div class="tweet-left">
-        <h3><a href="#">Thomas Durran</a> @TD</h3>
-        <p>Pellentesque tincidunt velit ac tellus malesuada, vel laoreet lectus dignissim.  Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. </p>
-      </div><!-- .tweet-left -->
+    </div>
+    <!-- #tab-one -->
+    
+    <div class="tab-content" id="tab-two">
+      <h2>Following</h2>
+      <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
+        <div class="tweet-left">
+          <h3><a href="#">Thomas Durran</a> @TD</h3>
+          <p>Pellentesque tincidunt velit ac tellus malesuada, vel laoreet lectus dignissim.  Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. </p>
+        </div>
+        <!-- .tweet-left -->
+        
+        <div class="tweet-right"> <a href="#" class="btn-small follow">unfollow</a> </div>
+        <!-- .tweet-right --> 
+        <br class="clr-flt">
+      </div>
+      <!-- .tweet -->
       
-      <div class="tweet-right"> <a href="#" class="btn-small follow">unfollow</a> </div><!-- .tweet-right --> 
-      <br class="clr-flt">
-    </div><!-- .tweet --> 
-
-
-    <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
-      <div class="tweet-left">
-        <h3><a href="#">Daisuke Murase</a> @typester</h3>
-        <p>Phiaculis nulla id purus convallis interdum. Sed aliquet consequat dui convallis venenatis. Nam sit amet arcu et tortor sodales semper.</p>
-      </div><!-- .tweet-left -->
+      <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
+        <div class="tweet-left">
+          <h3><a href="#">Daisuke Murase</a> @typester</h3>
+          <p>Phiaculis nulla id purus convallis interdum. Sed aliquet consequat dui convallis venenatis. Nam sit amet arcu et tortor sodales semper.</p>
+        </div>
+        <!-- .tweet-left -->
+        
+        <div class="tweet-right"> <a href="#" class="btn-small follow">unfollow</a> </div>
+        <!-- .tweet-right --> 
+        <br class="clr-flt">
+      </div>
+      <!-- .tweet -->
       
-      <div class="tweet-right"> <a href="#" class="btn-small follow">unfollow</a> </div><!-- .tweet-right --> 
-      <br class="clr-flt">
-    </div><!-- .tweet --> 
-
-
-
-    <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
-      <div class="tweet-left">
-        <h3><a href="#">David Hollander</a> @typester</h3>
-        <p>Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. Pellentesque tincidunt velit ac tellus malesuada, vel laoreet lectus dignissim.</p>
-      </div><!-- .tweet-left -->
+      <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
+        <div class="tweet-left">
+          <h3><a href="#">David Hollander</a> @typester</h3>
+          <p>Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. Pellentesque tincidunt velit ac tellus malesuada, vel laoreet lectus dignissim.</p>
+        </div>
+        <!-- .tweet-left -->
+        
+        <div class="tweet-right"><span class="date"> <a href="#" class="btn-small follow">unfollow</a> </div>
+        <!-- .tweet-right --> 
+        <br class="clr-flt">
+      </div>
+      <!-- .tweet --> 
       
-      <div class="tweet-right"><span class="date"> <a href="#" class="btn-small follow">unfollow</a> </div><!-- .tweet-right --> 
-      <br class="clr-flt">
-    </div><!-- .tweet --> 
-
-
-
-</div><!-- #tab-2 -->
-
-
-<div class="tab-content" id="tab-three">
-    <h2>Followers</h2>
-
-
-    <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
-      <div class="tweet-left">
-        <h3><a href="#">Thomas Durran</a> @TD</h3>
-        <p>Pellentesque tincidunt velit ac tellus malesuada, vel laoreet lectus dignissim.  Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. </p>
-      </div><!-- .tweet-left -->
+    </div>
+    <!-- #tab-2 -->
+    
+    <div class="tab-content" id="tab-three">
+      <h2>Followers</h2>
+      <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
+        <div class="tweet-left">
+          <h3><a href="#">Thomas Durran</a> @TD</h3>
+          <p>Pellentesque tincidunt velit ac tellus malesuada, vel laoreet lectus dignissim.  Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. </p>
+        </div>
+        <!-- .tweet-left -->
+        
+        <div class="tweet-right"> <a href="#" class="btn-small follow">follow</a> </div>
+        <!-- .tweet-right --> 
+        <br class="clr-flt">
+      </div>
+      <!-- .tweet -->
       
-      <div class="tweet-right"> <a href="#" class="btn-small follow">follow</a> </div><!-- .tweet-right --> 
-      <br class="clr-flt">
-    </div><!-- .tweet --> 
-
-
-    <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
-      <div class="tweet-left">
-        <h3><a href="#">Daisuke Murase</a> @typester</h3>
-        <p>Phiaculis nulla id purus convallis interdum. Sed aliquet consequat dui convallis venenatis. Nam sit amet arcu et tortor sodales semper.</p>
-      </div><!-- .tweet-left -->
+      <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
+        <div class="tweet-left">
+          <h3><a href="#">Daisuke Murase</a> @typester</h3>
+          <p>Phiaculis nulla id purus convallis interdum. Sed aliquet consequat dui convallis venenatis. Nam sit amet arcu et tortor sodales semper.</p>
+        </div>
+        <!-- .tweet-left -->
+        
+        <div class="tweet-right"> <a href="#" class="btn-small follow">follow</a> </div>
+        <!-- .tweet-right --> 
+        <br class="clr-flt">
+      </div>
+      <!-- .tweet -->
       
-      <div class="tweet-right"> <a href="#" class="btn-small follow">follow</a> </div><!-- .tweet-right --> 
-      <br class="clr-flt">
-    </div><!-- .tweet --> 
+      <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
+        <div class="tweet-left">
+          <h3><a href="#">David Hollander</a> @typester</h3>
+          <p>Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. Pellentesque tincidunt velit a</p>
+        </div>
+        <!-- .tweet-left -->
+        
+        <div class="tweet-right"> <a href="#" class="btn-small follow">follow</a> </div>
+        <!-- .tweet-right --> 
+        <br class="clr-flt">
+      </div>
+      <!-- .tweet --> 
+      
+    </div>
+    <!-- #tab-3 --> 
+    
+  </div>
+  <!-- .main-content --> 
+  
+</div>
+<!-- .wrapper --> 
 
-
-
-    <div class="tweet"> <img src="images/th-unk-user.png" width="48" height="48" alt="" class="user-th">
-      <div class="tweet-left">
-        <h3><a href="#">David Hollander</a> @typester</h3>
-        <p>Vestibulum at lectus ac enim suscipit mattis sit amet ut nunc. Pellentesque tincidunt velit a
-</div><!-- #tab-3 -->
-<!-- style="display:none"-->
-
-  </div><!-- .main-content -->
-   
-</div><!-- .wrapper -->
-
-
-<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
-
+<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script> 
 <script type="text/javascript">
 // <![CDATA[
 

a24bf54813d4382e3bbb7becf7bead50521b076f
Andrew Pearson <Andy@Grafixwerks.com>
2013-10-16 15:46:25 -0400

    rename files

--- application/views/view_footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-// site footer
-?>
-</div><!-- .wrapper -->
-
-<script type="text/javascript" src="<?php echo site_url(); ?>js/jquery-1.10.2.min.js"></script>
-<script type="text/javascript" src="<?php echo site_url(); ?>js/site.js"></script>
-</body>
-</html>
